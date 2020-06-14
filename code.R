4b3a91143947b198c29cb32ad92effb97a2dcb4c
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2020-06-14 22:57:03 +0800

    Update code.R

--- code.R
+++ code.R
@@ -1,3 +1,4 @@
+install.packages("jsonlite",dependencies=TRUE)
 baseurl <- function()"http://api.data.cma.cn:8090/api?"
 userId <- "5921014329184DMzs"
 pwd <- "O6Ex76U"
@@ -8,9 +9,8 @@ staIDs <- "56489,56497,56533,56543,56548"
 elements <- "Station_Id_C,Year,Mon,Day,Hour,PRS,PRS_Sea,PRS_Max,PRS_Min,TEM,TEM_Max,TEM_Min,RHU,RHU_Min,VAP,PRE_1h,WIN_D_INST_Max,WIN_S_Max,WIN_D_S_Max,WIN_S_Avg_2mi,WIN_D_Avg_2mi,WEP_Now,WIN_S_Inst_Max,tigan,windpower,VIS,CLO_Cov,CLO_Cov_Low,CLO_COV_LM"
 url = paste(baseurl(), "userId=", userId, "&pwd=",pwd,"&dataFormat=json&interfaceId=getSurfEleByTimeRangeAndStaID&dataCode=SURF_CHN_MUL_HOR&timeRange=",
             timeRange,"&staIDs=",staIDs,"&elements=",elements,sep = "")
-install.packages("jsonlite",dependencies=TRUE)
 db <- jsonlite::fromJSON(url, flatten=TRUE)
 if(db$returnMessage == "Query Succeed"){
-  path <- paste0("data/",Sys.time(),".rds")
+  path <- paste0(Sys.time(),".rds")
   saveRDS(db,path)
 }
