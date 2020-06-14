a9035c45ecf94c8f1386538beda03b90886f2b7f
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2020-06-14 14:49:09 +0800

    Update code.sh

--- code.sh
+++ code.sh
@@ -2,8 +2,6 @@
 set -e
 ## R
 # on Ubuntu
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
 sudo apt-get update
 sudo apt-get install r-base
 sudo apt-get install r-base-dev
