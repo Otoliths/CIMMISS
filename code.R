840c694c1fcfa6fcfeceb99dcfd1f43738f64765
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2020-06-16 04:16:31 +0800

    Update code.R

--- code.R
+++ code.R
@@ -1,6 +1,5 @@
 install.packages("curl")
 install.packages("httr")
-install.packages("readr")
 install.packages("jsonlite",dependencies=TRUE)
 baseurl <- function()"http://api.data.cma.cn:8090/api?"
 userId <- "5921014329184DMzs"
@@ -10,10 +9,8 @@ timeend <- paste0(gsub("-","",Sys.Date()),"000000")
 timeRange <- paste0("[",timestart,",",timeend,"]")
 staIDs <- "56489,56497,56533,56543,56548"
 elements <- "Station_Id_C,Year,Mon,Day,Hour,PRS,PRS_Sea,PRS_Max,PRS_Min,TEM,TEM_Max,TEM_Min,RHU,RHU_Min,VAP,PRE_1h,WIN_D_INST_Max,WIN_S_Max,WIN_D_S_Max,WIN_S_Avg_2mi,WIN_D_Avg_2mi,WEP_Now,WIN_S_Inst_Max,tigan,windpower,VIS,CLO_Cov,CLO_Cov_Low,CLO_COV_LM"
-url = paste(baseurl(), "userId=", userId, "&pwd=",pwd,"&dataFormat=json&interfaceId=getSurfEleByTimeRangeAndStaID&dataCode=SURF_CHN_MUL_HOR&timeRange=",
-            timeRange,"&staIDs=",staIDs,"&elements=",elements,sep = "")
+url <- paste0(baseurl(), "userId=", userId, "&pwd=",pwd,"&dataFormat=json&interfaceId=getSurfEleByTimeRangeAndStaID&dataCode=SURF_CHN_MUL_HOR&timeRange=",
+            timeRange,"&staIDs=",staIDs,"&elements=",elements)
 db <- jsonlite::fromJSON(url, flatten=TRUE)
-if(db$returnMessage == "Query Succeed"){
-            path <- paste0("data/",gsub("-","",as.Date(Sys.Date())),".rds")
-            readr::write_rds(db,path)
-}
+path <- paste0("data/",gsub("-","",as.Date(Sys.Date())),".rds")
+saveRDS(db,path)
