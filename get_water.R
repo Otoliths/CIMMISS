95b7b67f0d2388bda3ce3a11a891d48cd35bfbca
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2021-01-20 07:43:09 +0800

    Update get_water.R

--- get_water.R
+++ get_water.R
@@ -1,23 +1,25 @@
 #------------------------Custom get_occ function-------------
  get_data <- function(par,date){
-   par <- match.arg(par, c("water","rain"))
-   web <-"http://www.ynswj.cn/webapi/api/v1"
-   switch (par,
-           water = {
-             url = paste0(web,"/water?")
-             x <- jsonlite::fromJSON(url,flatten=TRUE)
-             x <- x[["data"]]
-             print(paste0(as.POSIXlt(Sys.time(), "Asia/Shanghai")," is OK !"))
-           },
-           rain = {
-             url = paste0(web,"/rain?")
-             x <- jsonlite::fromJSON(url,flatten=TRUE)
-             x <- x[["data"]]
-             print(paste0(as.POSIXlt(Sys.time(), "Asia/Shanghai")," is OK !"))
-           }
-   )
-   return(x)
- }
+  par <- match.arg(par, c("water","rain"))
+  web <-"http://www.ynswj.cn/webapi/api/v1"
+  switch (par,
+          water = {
+            url = paste0(web,"/water?")
+            urls <- url(url,"rb")
+            x <- jsonlite::fromJSON(urls,flatten=TRUE)
+            x <- x[["data"]]
+            print(paste0(as.POSIXlt(Sys.time(), "Asia/Shanghai")," is OK !"))
+          },
+          rain = {
+            url = paste0(web,"/rain?")
+            urls <- url(url,"rb")
+            x <- jsonlite::fromJSON(urls,flatten=TRUE)
+            x <- x[["data"]]
+            print(paste0(as.POSIXlt(Sys.time(), "Asia/Shanghai")," is OK !"))
+          }
+  )
+  return(x)
+}
 
  if (!file.exists(paste0("water/",as.POSIXlt(Sys.Date(), "Asia/Shanghai")))){
    dir.create(paste0("water/",as.POSIXlt(Sys.Date(), "Asia/Shanghai")))
